130d9d4dd9a44fa60ff4281395b2622019c22e20
[Aurelien] <[ademaille2@gmail.com]>
2025-07-27 15:51:22 +0200

    Script mit dans Dessin.html et tableaux js

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+const Animé = [
+    { name: "Freezer", image: "Dessin/Animés/Freezer.JPG", caption: "Freezer de DBZ" },
+    { name: "Cooler", image: "Dessin/Animés/Cooler.JPG", caption: "Cooler de DBZ" },
+    { name: "Vados", image: "Dessin/Animés/Vados.JPG", caption: "Vados de DBS" }
+]
+
+const Jeux = [
+    { name: "Vaylin", image: "Dessin/Jeux/Vaylin.JPG", caption: "Vaylin de SWOR" },
+    { name: "Yomi", image: "Dessin/Jeux/Yomi.JPG", caption: "Yomi de FF13-2" },
+    { name: "Untangle", image: "Dessin/Jeux/2.JPG", caption: "Énigme d'Untangle" }
+]
+console.log(Animé, Jeux);
