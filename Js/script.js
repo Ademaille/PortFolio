3e7a36fba33e966639a83a9c7cdce6c183c73ed7
[Aurelien] <[ademaille2@gmail.com]>
2025-08-04 15:47:07 +0200

    new file for url

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,49 @@
 const Animé = [
-    { name: "Freezer", image: "Dessin/Animés/Freezer.JPG", caption: "Freezer de DBZ" },
-    { name: "Cooler", image: "Dessin/Animés/Cooler.JPG", caption: "Cooler de DBZ" },
-    { name: "Vados", image: "Dessin/Animés/Vados.JPG", caption: "Vados de DBS" }
-]
+    { name: "Freezer", image: "Dessins/Animés/Freezer.JPG", caption: "Freezer de DBZ" },
+    { name: "Cooler", image: "Dessins/Animés/Cooler.JPG", caption: "Cooler de DBZ" },
+    { name: "Vados", image: "Dessins/Animés/Vados.JPG", caption: "Vados de DBS" }
+];
 
 const Jeux = [
-    { name: "Vaylin", image: "Dessin/Jeux/Vaylin.JPG", caption: "Vaylin de SWOR" },
-    { name: "Yomi", image: "Dessin/Jeux/Yomi.JPG", caption: "Yomi de FF13-2" },
-    { name: "Untangle", image: "Dessin/Jeux/2.JPG", caption: "Énigme d'Untangle" }
-]
-console.log(Animé, Jeux);
+    { name: "Vaylin", image: "Dessins/Jeux/Vaylin.JPG", caption: "Vaylin de SWOR" },
+    { name: "Yomi", image: "Dessins/Jeux/Yomi.JPG", caption: "Yomi de FF13-2" },
+    { name: "Untangle", image: "Dessins/Jeux/2.JPG", caption: "Énigme d'Untangle" }
+];
+
+const container = document.createElement('div');
+container.className = "dessin";
+
+
+  const h2 = document.createElement('h2');
+  h2.textContent = Animés;
+  container.appendChild(h2);
+
+  const dessinContainer = document.createElement('div');
+  dessinContainer.className = "dessin-container";
+  dessinContainer.id = Animés;
+
+  images.forEach(image => {
+    const figure = document.createElement('figure');
+
+    const a = document.createElement('a');
+    a.href = image.src;
+    a.target = "_blank";
+
+    const img = document.createElement('img');
+    img.src = image.src;
+    img.className = "lien-img";
+
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = image.caption;
+
+    a.appendChild(img);
+    figure.appendChild(a);
+    figure.appendChild(figcaption);
+    dessinContainer.appendChild(figure);
+  });
+
+  container.appendChild(dessinContainer);
+
+document.body.appendChild(container);
+
+
